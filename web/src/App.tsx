ff1071429bfa10c2f779ac4b2396c1564a4490ef
Lucas Costa <lucas.co090869@gmail.com>
2024-07-09 14:23:45 -0300

    chore: add home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,44 @@
+import { MapPin, Calendar, ArrowRight } from "lucide-react";
+
+/**
+ * Renders the main application component.
+ *
+ * @return {JSX.Element} The rendered application component.
+ */
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
+      <div className="max-w-3xl w-full px-6 text-center space-y-10">
+        <div className="flex flex-col items-center gap-3">
+          <img src="/logo.svg" alt="plann.er" />
+          <p className="text-zinc-300 text-lg">
+            Call your friends to plan a trip together!
+          </p>
+        </div>
+        
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+          <div className="flex items-center gap-2 flex-1">
+            <MapPin className="size-5 text-zinc-400"/>
+            <input type="text" placeholder="Where are we going?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Calendar className="size-5 text-zinc-400"/>
+            <input type="text" placeholder="When" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"/>
+          </div>
+
+          <div className="w-px h-6 bg-zinc-800" />
+
+          <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            Continue
+            <ArrowRight className="size-5" />
+          </button>
+        </div>
+
+        <p className="text-sm text-zinc-500">
+          To plan your trip using plann.ner you automatically agree <br/> with our <a className="text-zinc-300 underline" href="#">Terms of Service</a> and <a className="text-zinc-300 underline" href="#">Privacy Policy</a>
+        </p>
+      </div>
+    </div>
   )
-}
+} 
